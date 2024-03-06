627a7d1c567d1e07d2647660debb2b1c3e0d947c
JoseMagno20 <joseotaviomagno@gmail.com>
2024-03-06 17:42:54 -0300

    Primeiro commit na Tarefa da EBAC

---
+++ src/magno/PrimeiraClasse.java
@@ -0,0 +1,17 @@
+package magno;
+
+public class PrimeiraClasse {
+ public static void main(String[] args) {
+        System.out.print("Vasco do Gama!");
+        String nome = "José Otávio Barros Magno";
+        int idade = 24;
+        String curso = " Backend Java";
+
+        System.out.println("Meu nome é "+ nome + " tenho "+idade+" anos, e estou fazendo um curso na EBAC de "+curso+" ,NOTA 10 o curso");
+
+        
+
+
+
+    }
+}
